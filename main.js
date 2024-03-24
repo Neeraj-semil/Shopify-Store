7456a7764af4f0108a34611c5cc61c8ac2b7269e
Neeraj Semil <semilneeraj2001@gmail.com>
2024-03-24 10:48:38 +0530

    add files

---
+++ main.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+import products from './api/products.json';
+
+// call the function to display products as a card
+
+import { showProductContainer } from "./homeProductCards";
+
+// Define a function named `showProductContainer` that takes an array of products as input.
+showProductContainer(products);
+
+
